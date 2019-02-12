7efbc6338e25bd1af452d93d0d174d625a328536
John Doe <cdplatat@uqvirtual.edu.co>
2019-02-12 13:25:42 -0500

    uno

---
+++ app/User.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace InterSoluciones;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    public function roles(){
+        return $this->belongsToMany('InterSoluciones\Role');
+
+
+    }
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
